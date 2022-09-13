062068a9986e324a203aad5e26b0b28d4a50d934
Max <himmelmax@gmail.com>
2022-09-13 09:50:55 -0700

    create gameplay settings
    contains player movement settings

---
+++ Assets/Scripts/GameplaySettings.cs
@@ -0,0 +1,23 @@
+using TopDown.Gameplay.Movement;
+using UnityEngine;
+using Zenject;
+
+namespace TopDown.Gameplay
+{
+    [CreateAssetMenu]
+    public class GameplaySettings : ScriptableObjectInstaller
+    {
+        [SerializeField] private PlayerSettings _playerSettings;
+
+        [System.Serializable]
+		public class PlayerSettings
+		{
+            public CharacterMotor.Settings Movement;
+		}
+
+		public override void InstallBindings()
+		{
+			Container.BindInstance( _playerSettings );
+		}
+	}
+}
